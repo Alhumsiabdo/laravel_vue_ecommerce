161f2defd66784969b0ffa693a85591cbda124d2
abdullah <abdullah.alhumsi9@gmail.com>
2023-12-31 08:58:25 +0200

    work in add product

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -2,10 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class AdminSeeder extends Seeder
 {
